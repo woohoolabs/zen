f98abb52f2ba3735affcb52dd4d8ced989da3edd
kocsismate <kocsismate@woohoolabs.com>
2019-08-14 01:06:47 +0200

    Add support for preloading individual classes

---
+++ src/Config/Preload/ClassPreload.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace WoohooLabs\Zen\Config\Preload;
+
+class ClassPreload extends AbstractPreload
+{
+    /**
+     * @var string
+     */
+    private $className;
+
+    public static function create(string $className): ClassPreload
+    {
+        return new ClassPreload($className);
+    }
+
+    public function __construct(string $className)
+    {
+        $this->className = $className;
+    }
+
+    /**
+     * @return string[]
+     * @internal
+     */
+    public function getClassNames(): array
+    {
+        return [
+            $this->className,
+        ];
+    }
+}
