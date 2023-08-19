7d933a2329257a8002f2c3ba02f200f5a8d7d31c
Máté Kocsis <kocsismate@woohoolabs.com>
2023-08-19 17:28:45 +0200

    Revert check for existence of a property

--- src/AbstractCompiledContainer.php
+++ src/AbstractCompiledContainer.php
@@ -7,8 +7,6 @@ namespace WoohooLabs\Zen;
 use Closure;
 use Psr\Container\ContainerInterface;
 
-use function property_exists;
-
 abstract class AbstractCompiledContainer implements ContainerInterface
 {
     /** @var array<string, object> */
@@ -20,9 +18,7 @@ abstract class AbstractCompiledContainer implements ContainerInterface
         Closure::bind(
             static function () use ($object, $properties): void {
                 foreach ($properties as $name => $value) {
-                    if (property_exists($object, $name)) {
-                        $object->$name = $value;
-                    }
+                    $object->$name = $value;
                 }
             },
             null,
